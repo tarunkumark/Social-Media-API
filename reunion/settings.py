36f1a38a317e6a42257da91c289421c2ef736efe
Tarun K Kumar <tkksctwo@gmail.com>
2023-04-11 21:59:39 +0530

    trying os.getenv

--- reunion/settings.py
+++ reunion/settings.py
@@ -77,17 +77,20 @@ TEMPLATES = [
 WSGI_APPLICATION = "reunion.wsgi.application"
 
 
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
         'PORT': '5432',
     }
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
